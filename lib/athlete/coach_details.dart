77bfcd5afac60f07933b998bf5c29e74e86d0839
pr1266 <pourya_1999@yahoo.com>
2020-01-08 11:56:48 -0800

    'athlete_services'

--- lib/athlete/coach_details.dart
+++ lib/athlete/coach_details.dart
@@ -16,19 +16,62 @@ class CoachDetails extends StatelessWidget{
       child: Container(
         width: 140,
         height: 140,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/1.jpg'),
+            fit: BoxFit.cover
+          ),
+          borderRadius: BorderRadius.all(Radius.circular(80)),
+          border: Border.all(
+            color: Colors.white,
+            width: 10,
+          )
+        ),
       ),
     );
   }
 
+  Widget _buildStatus(BuildContext context){
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 4.0, horizontal: 6.0),
+      decoration: BoxDecoration(
+        color: Theme.of(context).scaffoldBackgroundColor,
+        borderRadius: BorderRadius.all(Radius.circular(4.0))
+      ),
+      child: Text(
+        degree,
+        style: TextStyle(
+          color: Colors.black,
+          fontSize: 20.0,
+          fontWeight: FontWeight.w300
+        ),
+      ),
+    );
+  }
+  
   Widget _buildCoverImage(Size screensize){
     return Container(
       height: screensize.height/2.6,
       decoration: BoxDecoration(
+        color: Colors.blue,
         image: DecorationImage(
           image: AssetImage('assets/1.jpg'),
           fit: BoxFit.cover
         )
       ),
+    );
+  }
+
+  Widget _buildFullName(){
+    TextStyle _nameStyle = TextStyle(
+      color: Colors.black,
+      fontSize: 28.0,
+      fontWeight: FontWeight.bold
+    );
+
+    return Text(
+      fullname,
+      style: _nameStyle,
     );
   }
 
@@ -40,6 +83,20 @@ class CoachDetails extends StatelessWidget{
       body: Stack(
         children: <Widget>[
           _buildCoverImage(screensize),
+          SafeArea(
+            child: SingleChildScrollView(
+              child: Column(
+                children: <Widget>[
+                  SizedBox(
+                    height: screensize.height/6.4,
+                  ),
+                  _buildProfileImage(),
+                  _buildFullName(),
+                  _buildStatus(context),
+                ],
+              ),
+            ),
+          ),
         ],
       ),
     );
