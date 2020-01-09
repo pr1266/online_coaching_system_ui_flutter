904e6eb08bb376c594b76225650b98bd94a0c4f4
pr1266 <pourya_1999@yahoo.com>
2020-01-08 16:23:23 -0800

    'coach_detail_page'

--- lib/athlete/coach_details.dart
+++ lib/athlete/coach_details.dart
@@ -1,6 +1,7 @@
 import 'package:circular_profile_avatar/circular_profile_avatar.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:http/http.dart';
 
 
@@ -14,14 +15,14 @@ class CoachDetails extends StatelessWidget{
   Widget _buildProfileImage(){
     return Center(
       child: Container(
-        width: 140,
-        height: 140,
+        width: 200,
+        height: 200,
         decoration: BoxDecoration(
           image: DecorationImage(
             image: AssetImage('assets/1.jpg'),
             fit: BoxFit.cover
           ),
-          borderRadius: BorderRadius.all(Radius.circular(80)),
+          borderRadius: BorderRadius.all(Radius.circular(100)),
           border: Border.all(
             color: Colors.white,
             width: 10,
@@ -49,15 +50,12 @@ class CoachDetails extends StatelessWidget{
     );
   }
   
-  Widget _buildCoverImage(Size screensize){
+  Widget _buildCoverImage(Size screensize, BuildContext context){
     return Container(
-      height: screensize.height/2.6,
+      height: MediaQuery.of(context).size.height * .22,
       decoration: BoxDecoration(
-        color: Colors.blue,
-        image: DecorationImage(
-          image: AssetImage('assets/1.jpg'),
-          fit: BoxFit.cover
-        )
+        color: Colors.white,
+        borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20), bottomRight: Radius.circular(20))
       ),
     );
   }
@@ -79,26 +77,220 @@ class CoachDetails extends StatelessWidget{
   Widget build(BuildContext context) {
     // TODO: implement build
     Size screensize = MediaQuery.of(context).size;
-    return Scaffold(
-      body: Stack(
+    return Container(
+      child: new ListView(
         children: <Widget>[
-          _buildCoverImage(screensize),
-          SafeArea(
-            child: SingleChildScrollView(
-              child: Column(
-                children: <Widget>[
-                  SizedBox(
-                    height: screensize.height/6.4,
-                  ),
-                  _buildProfileImage(),
-                  _buildFullName(),
-                  _buildStatus(context),
-                ],
-              ),
-            ),
+          new Stack(
+            children: <Widget>[
+              _buildCoverImage(screensize, context),
+              new Align(
+                child: _buildProfileImage(),
+                alignment: Alignment.topRight,
+              )
+            ],
           ),
+          new Container(
+            height: 50,
+          ),
+          new Container(
+            margin: EdgeInsets.only(left: 20, right: 20),
+            height: MediaQuery.of(context).size.height * .5,
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.all(Radius.circular(20)),
+            ),
+            child: new ListView(
+              shrinkWrap: true,
+              children: <Widget>[
+                new Container(
+                  height: 20,
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    new Container(
+                      padding: EdgeInsets.only(right: 15),
+                      color: Colors.white,
+                      height: 40,
+                      child: new Text(
+                        'نام :',
+                        style: TextStyle(
+                          fontSize: 20,
+                          fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      padding: EdgeInsets.only(right: 15),
+                      color: Colors.white,
+                      height: 40,
+                      child: new Text(
+                          ' هادی چوپان',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    new Container(
+                      padding: EdgeInsets.only(right: 15),
+                      color: Colors.white,
+                      height: 40,
+                      child: new Text(
+                        'مدرک :',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      padding: EdgeInsets.only(right: 15),
+                      color: Colors.white,
+                      height: 40,
+                      child: new Text(
+                        'لیسانس تربیت بدنی',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                new Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    new Container(
+                      padding: EdgeInsets.only(right: 15),
+                      color: Colors.white,
+                      height: 40,
+                      child: new Text(
+                        'شهر :',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                    new Container(
+                      padding: EdgeInsets.only(right: 15),
+                      color: Colors.white,
+                      height: 40,
+                      child: new Text(
+                        'شیراز',
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+                new Container(
+                  height: 40,
+                  padding: EdgeInsets.only(right: 15),
+                  child: new Text(
+                    'افتخارات',
+                    style: new TextStyle(
+                      color: Colors.black,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 30
+                    ),
+                  ),
+                ),
+                // TODO eftekharat:
+                new Container(
+                  height: 30,
+                  margin: EdgeInsets.only(top: 20),
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        padding: EdgeInsets.only(right: 15),
+                        child: Icon(Icons.add, color: Colors.black, ),
+                      ),
+                      new Container(
+                        padding: EdgeInsets.only(right: 10),
+                        child: new Text(
+                          'قهرمان مستر المپیا حرفه ای 2019',
+                          style: new TextStyle(
+                            color: Colors.black,
+                            fontSize: 20
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+                // TODO eftekharat
+                new Container(
+                  height: 30,
+                  margin: EdgeInsets.only(top: 20),
+                  child: new Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: <Widget>[
+                      new Container(
+                        padding: EdgeInsets.only(right: 15),
+                        child: Icon(Icons.add, color: Colors.black, ),
+                      ),
+                      new Container(
+                        padding: EdgeInsets.only(right: 10),
+                        child: new Text(
+                          'قهرمان مستر المپیا آماتوری 2015',
+                          style: new TextStyle(
+                              color: Colors.black,
+                              fontSize: 20
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
         ],
       ),
     );
   }
 }
+
+
+//return Scaffold(
+//body: Stack(
+//children: <Widget>[
+//_buildCoverImage(screensize, context),
+//SafeArea(
+//child: SingleChildScrollView(
+//child: Column(
+//children: <Widget>[
+//SizedBox(
+//height: screensize.height/7,
+//),
+//_buildProfileImage(),
+////_buildFullName(),
+////_buildStatus(context),
+//Container(
+//height: MediaQuery.of(context).size.height * .1,
+//),
+//Container(
+//height: MediaQuery.of(context).size.height * .5,
+//margin: EdgeInsets.only(left: 20, right: 20),
+//decoration: BoxDecoration(
+//borderRadius: BorderRadius.all(Radius.circular(20)),
+//color: Colors.blue
+//),
+//)
+//],
+//),
+//),
+//),
+//],
+//),
+//);
--- lib/services/athlete.dart
+++ lib/services/athlete.dart
@@ -1,60 +1,60 @@
-import 'dart:convert';
-import 'package:http/http.dart';
-
-class Services{
-  Future<Map> myCoaches(String nat_code, Map<String, String> Header) async{
-    //TODO inja request midim
-    var body = {
-      'nat_code': nat_code
-    };
-    var response = await post('http://10.0.2.2:8000/', body: body, headers: Header);
-    var responsebody = json.decode(response.body);
-    //TODO inja model sazi mikonim
-    return {
-      'coaches':
-    };
-  }
-
-  Future<Map> coachDetails(String nat_code, Map<String, String> Header) async{
-    var body = {
-      'nat_code': nat_code
-    };
-    var response = await post('http://10.0.2.2:8000/', body: body, headers: Header);
-    var responsebody = json.decode(response.body);
-
-    return {
-      'coach_details': responsebody
-    };
-  }
-
-  Future<int> createContract(String Athlete, String Coach, Map<String, String> Header) async{
-    var body = {
-      'athlete': Athlete,
-      'coach': Coach,
-    };
-    var response = await post('http://10.0.2.2:8000/', body: body, headers: Header);
-    return response.statusCode;
-  }
-
-  Future<Map> search(String cityID, Map<String, String> Header) async{
-    var response = await get('http://10.0.2.2:8000/', headers: Header);
-    var responsebody = json.decode(response.body);
-    //TODO inja model sazi
-
-    return {
-      'coaches':
-    };
-  }
-
-  Future<Map> getCity(Map<String, String> Header) async{
-    var response = await get('http://10.0.2.2:8000/', headers: Header);
-    var responsebody = json.decode(response.body);
-
-    //inja bayad model sazi koni
-    return {
-      'city':
-    };
-  }
-
-
-}
+//import 'dart:convert';
+//import 'package:http/http.dart';
+//
+//class Services{
+//  Future<Map> myCoaches(String nat_code, Map<String, String> Header) async{
+//    //TODO inja request midim
+//    var body = {
+//      'nat_code': nat_code
+//    };
+//    var response = await post('http://10.0.2.2:8000/', body: body, headers: Header);
+//    var responsebody = json.decode(response.body);
+//    //TODO inja model sazi mikonim
+//    return {
+//      'coaches':
+//    };
+//  }
+//
+//  Future<Map> coachDetails(String nat_code, Map<String, String> Header) async{
+//    var body = {
+//      'nat_code': nat_code
+//    };
+//    var response = await post('http://10.0.2.2:8000/', body: body, headers: Header);
+//    var responsebody = json.decode(response.body);
+//
+//    return {
+//      'coach_details': responsebody
+//    };
+//  }
+//
+//  Future<int> createContract(String Athlete, String Coach, Map<String, String> Header) async{
+//    var body = {
+//      'athlete': Athlete,
+//      'coach': Coach,
+//    };
+//    var response = await post('http://10.0.2.2:8000/', body: body, headers: Header);
+//    return response.statusCode;
+//  }
+//
+//  Future<Map> search(String cityID, Map<String, String> Header) async{
+//    var response = await get('http://10.0.2.2:8000/', headers: Header);
+//    var responsebody = json.decode(response.body);
+//    //TODO inja model sazi
+//
+//    return {
+//      'coaches':
+//    };
+//  }
+//
+//  Future<Map> getCity(Map<String, String> Header) async{
+//    var response = await get('http://10.0.2.2:8000/', headers: Header);
+//    var responsebody = json.decode(response.body);
+//
+//    //inja bayad model sazi koni
+//    return {
+//      'city':
+//    };
+//  }
+//
+//
+//}
